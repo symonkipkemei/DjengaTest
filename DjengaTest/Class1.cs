71139f219173f7330f71fe0bacd490e08a998d67
User <User@DESKTOP-RPLDG13>
2023-10-23 21:39:22 +0300

    Add project files.

---
+++ DjengaTest/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+
+namespace DjengaTest
+{
+    [Transaction(TransactionMode.Manual)]
+    public class wallFinishes: IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            return Result.Succeeded;
+        }
+
+    }
+    
+}
