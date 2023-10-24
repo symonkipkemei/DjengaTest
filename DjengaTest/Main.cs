1022509d3396a54c29f6f4af15432e4a36f3977b
Symon Kipkemei <symonkipkemei@gmail.com>
2023-10-24 04:58:43 +0300

    Task dialog box added

--- DjengaTest/Main.cs
+++ DjengaTest/Main.cs
@@ -11,7 +11,7 @@ using Autodesk.Revit.ApplicationServices;
 namespace DjengaTest
 {
     [Transaction(TransactionMode.Manual)]
-    public class wallFinishes: IExternalCommand
+    public class WallCollector: IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -19,6 +19,16 @@ namespace DjengaTest
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
+
+            // create collector object 
+            FilteredElementCollector collector = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Walls)
+                .WhereElementIsNotElementType();
+
+            //Display information on a task dialog box
+            var simpleForm = new SimpleForm(collector);
+            simpleForm.ShowDialog();
+
 
             return Result.Succeeded;
         }
